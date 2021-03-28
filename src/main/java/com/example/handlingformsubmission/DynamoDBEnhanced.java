f441a5bc3be8ca49f4e6cb73740732bb1e85c5f5
Pádua Alves <paduaalves03@gmail.com>
2021-03-27 21:29:51 -0300

    Construindo a classe responsável por salvar as informações

--- src/main/java/com/example/handlingformsubmission/DynamoDBEnhanced.java
+++ src/main/java/com/example/handlingformsubmission/DynamoDBEnhanced.java
@@ -1,9 +1,120 @@
 package com.example.handlingformsubmission;
 
+import static software.amazon.awssdk.enhanced.dynamodb.mapper.StaticAttributeTags.primaryPartitionKey;
+import software.amazon.awssdk.auth.credentials.EnvironmentVariableCredentialsProvider;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.StaticTableSchema;
+import software.amazon.awssdk.enhanced.dynamodb.model.PutItemEnhancedRequest;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.model.ProvisionedThroughput;
 import org.springframework.stereotype.Component;
 
 @Component("DynamoDBEnhanced")
 public class DynamoDBEnhanced {
-    public void injectDynamoItem(Greeting greeting) {
+
+    private final ProvisionedThroughput DEFAULT_PROVISIONED_THROUGHPUT =
+            ProvisionedThroughput.builder()
+                    .readCapacityUnits(50L)
+                    .writeCapacityUnits(50L)
+                    .build();
+
+    private final TableSchema<GreetingItems> TABLE_SCHEMA =
+            StaticTableSchema.builder(GreetingItems.class)
+                    .newItemSupplier(GreetingItems::new)
+                    .addAttribute(String.class, a -> a.name("idblog")
+                            .getter(GreetingItems::getId)
+                            .setter(GreetingItems::setId)
+                            .tags(primaryPartitionKey()))
+                    .addAttribute(String.class, a -> a.name("author")
+                            .getter(GreetingItems::getName)
+                            .setter(GreetingItems::setName))
+                    .addAttribute(String.class, a -> a.name("title")
+                            .getter(GreetingItems::getTitle)
+                            .setter(GreetingItems::setTitle))
+                    .addAttribute(String.class, a -> a.name("body")
+                            .getter(GreetingItems::getMessage)
+                            .setter(GreetingItems::setMessage))
+                    .build();
+
+    // Uses the enhanced client to inject a new post into a DynamoDB table
+    public void injectDynamoItem(Greeting item){
+
+        Region region = Region.US_EAST_1;
+        DynamoDbClient ddb = DynamoDbClient.builder()
+                .region(region)
+                .credentialsProvider(EnvironmentVariableCredentialsProvider.create())
+                .build();
+
+        try {
+
+            DynamoDbEnhancedClient enhancedClient = DynamoDbEnhancedClient.builder()
+                    .dynamoDbClient(ddb)
+                    .build();
+
+            // Create a DynamoDbTable object
+            DynamoDbTable<GreetingItems> mappedTable = enhancedClient.table("Greeting", TABLE_SCHEMA);
+            GreetingItems gi = new GreetingItems();
+            gi.setName(item.getName());
+            gi.setMessage(item.getBody());
+            gi.setTitle(item.getTitle());
+            gi.setId(item.getId());
+
+            PutItemEnhancedRequest enReq = PutItemEnhancedRequest.builder(GreetingItems.class)
+                    .item(gi)
+                    .build();
+
+            mappedTable.putItem(enReq);
+
+        } catch (Exception e) {
+            e.getStackTrace();
+        }
+    }
+
+    public class GreetingItems {
+
+        // Set up data members that correspond to columns in the Work table
+        private String id;
+        private String name;
+        private String message;
+        private String title;
+
+        public GreetingItems()
+        {
+        }
+
+        public String getId() {
+            return this.id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getMessage(){
+            return this.message;
+        }
+
+        public void setMessage(String message){
+            this.message = message;
+        }
+
+        public String getTitle() {
+            return this.title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
     }
 }
--- src/main/java/com/example/handlingformsubmission/Greeting.java
+++ src/main/java/com/example/handlingformsubmission/Greeting.java
@@ -1,4 +1,12 @@
 package com.example.handlingformsubmission;
 
+import lombok.Data;
+
+@Data
 public class Greeting {
+    private String id;
+    private String body;
+    private String name;
+    private String title;
 }
+
